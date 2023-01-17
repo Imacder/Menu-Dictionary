df85400028e4a8100e25ca7145fa11fa8d108f89
Imacder <sunlili209@gmail.com>
2023-01-17 21:11:26 +0800

    Initial Commit

---
+++ Menu Dictionary/Menu_DictionaryApp.swift
@@ -0,0 +1,17 @@
+//
+//  Menu_DictionaryApp.swift
+//  Menu Dictionary
+//
+//  Created by Albert Kong on 2023/1/17.
+//
+
+import SwiftUI
+
+@main
+struct Menu_DictionaryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Menu DictionaryUITests/Menu_DictionaryUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  Menu_DictionaryUITestsLaunchTests.swift
+//  Menu DictionaryUITests
+//
+//  Created by Albert Kong on 2023/1/17.
+//
+
+import XCTest
+
+final class Menu_DictionaryUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
